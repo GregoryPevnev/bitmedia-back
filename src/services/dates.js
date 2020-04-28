f17e784e25532128cc0040f60828afb01e4ba7fb
Hryhorii Pievniev <pyevnyev@sheridancollege.ca>
2020-04-28 15:04:37 +0300

    Dates

--- src/services/dates.js
+++ src/services/dates.js
@@ -1,5 +1,18 @@
-exports.validateDates = (start, end) => {
-  // TODO: Validation (Pure)
+const DATE_FORMAT = /^\d{4}-\d{2}-\d{2}$/
 
-  return [];
+const isDate = date => DATE_FORMAT.test(date) && !Number.isNaN(date.getTime());
+
+const isRange = (startDate, endDate) => startDate.getTime() < endDate.getTime();
+
+exports.validateDates = (start, end) => {
+  const errors = [];
+
+  const startDate = new Date(start), endDate = new Date(end);
+
+  if (!isDate(startDate)) errors.push("Invalid starting date");
+  if (!isDate(endDate)) errors.push("Invalid ending date");
+
+  if (!isRange(startDate, endDate)) errors.push("Invalid range of dates");
+
+  return errors;
 };
